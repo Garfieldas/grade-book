96e49844a632b9ac20ad87d571649e8659ad4a00
Garieldas <dainiusrainys99@gmail.com>
2025-11-09 18:29:45 +0200

    Unregistered groups class from django admin. Created custom user class for django admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from commons.admin import BaseAdmin
+from users.models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseAdmin):
+    fieldsets = (
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Password", {"fields": ("password",)}),
+        ("Status", {"fields": ("role", "is_active", "is_staff",)}),
+        ("Additional Info", {"fields": ("date_joined", "last_login",)}),
+    )
+
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "role",
+        "is_mentor"
+    )
+    readonly_fields = ("date_joined", "last_login")
+
+    list_filter = ("role", "is_mentor", "is_staff")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ('last_name', 'first_name')
