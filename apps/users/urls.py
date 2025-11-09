dbaedeb62cae9ee908fd34c78d9995041ab2b8eb
Garieldas <dainiusrainys99@gmail.com>
2025-11-09 22:45:04 +0200

    Created login and logout views

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login')
+    path('', views.LoginView, name='login'),
+    path('/logout', views.LogoutView, name='logout')
 ]
