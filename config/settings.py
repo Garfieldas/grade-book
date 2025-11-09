28bce0568f304ed1f848c91801fa8e06846c67c6
Garieldas <dainiusrainys99@gmail.com>
2025-11-09 21:38:10 +0200

    Created base template directory

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'commons',
-    'users'
+    'users',
+    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
