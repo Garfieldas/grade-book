144b65d1cd3610dbe69a1b1eb59721a67dadd11d
Garieldas <dainiusrainys99@gmail.com>
2025-11-10 10:52:35 +0200

    Added error message display to form

--- apps/users/views.py
+++ apps/users/views.py
@@ -13,16 +13,13 @@ def LoginView(request):
             email = form.cleaned_data["email"]
             password = form.cleaned_data["password"]
             user = authenticate(request, email=email, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        messages.error(request, "Invalid email or password.")
-
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            messages.error(request, "Invalid email or password.")
     else:
         form = UserLoginForm()
-        context = {"form": form}
-    return render(request, 'users/login.html', context)
+    return render(request, 'users/login.html', {"form": form})
 
 def LogoutView(request):
     logout(request)
