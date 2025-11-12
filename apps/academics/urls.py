cce96cfca81f5c087caaf6cd107bc4421ba37a2d
Garieldas <dainiusrainys99@gmail.com>
2025-11-12 21:29:21 +0200

    Removed schedule from project

--- apps/academics/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('student', views.student_schedule, name='student_schedule'),
-    path('teacher', views.teacher_schedule, name='teacher_schedule')
-]
--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('home/', include('dashboard.urls')),
-    path('schedule', include('academics.urls')),
 ]
