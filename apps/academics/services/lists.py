8180ddb03fcc7ecf8b10dee04e5bd55e965b9900
Garieldas <dainiusrainys99@gmail.com>
2025-11-12 22:00:00 +0200

    Confclits

--- apps/academics/services/lists.py
+++ apps/academics/services/lists.py
@@ -5,9 +5,6 @@ def get_student_class(user: User):
     student_class = StudentClass.objects.filter(student=user).first()
     return student_class.school_class
 
-def get_student_lessons(school_class, day):
-    student_lessons = (
-        Schedule.objects.select_related("subject__teacher")
-        .filter(school_class=school_class, day=day)
-    )
-    return student_lessons
+def get_student_teachers(school_class):
+    pass
+    
