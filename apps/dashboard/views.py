785276e71bd4ce32b3912165b43e663ed75612cf
Garieldas <dainiusrainys99@gmail.com>
2025-11-12 22:04:07 +0200

    conflicts

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -4,10 +4,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     return render(request, "dashboard/index.html")
-<<<<<<< HEAD
 
 @login_required
 def dashboard(request):
     return render(request, "dashboard/dashboard.html")
-=======
->>>>>>> development
