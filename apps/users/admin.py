3026d274b505717680b1919cfe3f77769f66a9d7
Garieldas <dainiusrainys99@gmail.com>
2025-11-14 19:09:04 +0200

    Added password hashing for user admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -24,3 +24,15 @@ class UserAdmin(BaseAdmin):
     list_filter = ("role", "is_mentor", "is_staff")
     search_fields = ("first_name", "last_name", "email")
     ordering = ('last_name', 'first_name')
+
+    def save_model(self, request, obj, form, change):
+        password = form.cleaned_data.get('password')
+        if password and 'password' in form.changed_data:
+            obj.set_password(form.cleaned_data['password'])
+        super().save_model(request, obj, form, change)
+
+    def update(self, request, obj, form, change):
+        password = form.cleaned_data.get('password')
+        if password and 'password' in form.changed_data:
+            obj.set_password(form.cleaned_data['password'])
+        super().update(request, obj, form, change)
